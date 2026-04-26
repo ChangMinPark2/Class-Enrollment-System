b3a3266c6a4cab206cc911ccdecdce7caf90d044
CHANG MIN PARK <98qkrckdals@naver.com>
2026-04-27 03:16:29 +0900

    refactor: 클래스명 변경

--- src/main/java/com/example/demo/api/controller/CourseCommandController.java
+++ src/main/java/com/example/demo/api/controller/CourseCommandController.java
@@ -1,31 +1,34 @@
 package com.example.demo.api.controller;
 
-import com.example.demo.api.dto.course.CourseReadAllDto;
-import com.example.demo.api.dto.course.CourseReadDetailDto;
-import com.example.demo.api.persistence.entity.CourseStatus;
+import com.example.demo.api.dto.course.CourseCreateDto;
 import com.example.demo.api.service.CourseService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/course")
+@RequestMapping("/api/users")
 public class CourseCommandController {
     private final CourseService courseService;
 
-    @GetMapping
-    public ResponseEntity<CourseReadAllDto> readAll(
-            @RequestParam(required = false) CourseStatus status
+    @PostMapping("/{userId}/courses")
+    public ResponseEntity<String> create(
+            @PathVariable Long userId,
+            @RequestBody @Valid CourseCreateDto dto
     ) {
-        return ResponseEntity.ok(courseService.readAll(status));
+        courseService.create(userId, dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("강의가 생성되었습니다.");
     }
 
-    @GetMapping("/{courseId}")
-    public ResponseEntity<CourseReadDetailDto> read(
+    @PostMapping("/{userId}/courses/{courseId}/open")
+    public ResponseEntity<String> open(
+            @PathVariable Long userId,
             @PathVariable Long courseId
     ) {
-        return ResponseEntity.ok(courseService.read(courseId));
+        courseService.open(userId, courseId);
+        return ResponseEntity.ok("강의가 오픈되었습니다.");
     }
 }
