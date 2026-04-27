f2774dcec5f82683f46953656b4c64d6f3ff2d73
CHANG MIN PARK <98qkrckdals@naver.com>
2026-04-28 05:34:38 +0900

    hotfix: 에러 수정

--- src/main/java/com/example/demo/api/controller/CourseReadController.java
+++ src/main/java/com/example/demo/api/controller/CourseReadController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/course")
+@RequestMapping("/api/courses")
 public class CourseReadController {
     private final CourseService courseService;
 
